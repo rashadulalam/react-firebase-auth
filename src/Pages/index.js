886097400dccb22cfee74e17be34f11aec478922
rashadulalam <rashadul91@gmail.com>
2023-02-24 12:48:30 +0600

    Page created

---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+export {default as Home} from './Home/Home';
+export {default as About} from './About/About';
+export {default as Services} from './Services/Services';
+export {default as Signin} from './Signin/Signin';
+export {default as Signup} from './Signup/Signup';
+export {default as NotFound} from './NoteFound/NotFound';
